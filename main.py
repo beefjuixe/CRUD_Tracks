1b46e8cdeeedf24e700561fba7918879105626f4
Rajasekar S <mithunj09@gmail.com>
2025-05-11 09:12:32 -0700

    Added PUT and DELETE endpoints

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI, Response
 from models import Track
 from contextlib import asynccontextmanager
 
-
 data = []
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,7 +14,7 @@ async def lifespan(app: FastAPI):
         tracks = json.load(f)
         for track in tracks:
             parsed = Track(**track)
-            print("Parsed model:", parsed)
+            #print("Parsed model:", parsed)
             data.append(parsed)
     yield
 
@@ -25,7 +24,7 @@ def track():
     return data
 
 @app.get('/tracks/{track_id}' , response_model = Union[Track,str])
-def track(track_id: int, response: Response):
+def get_track(track_id: int, response: Response):
     track = None
     for t in data:
         if t.id == track_id:
@@ -35,3 +34,40 @@ def track(track_id: int, response: Response):
         response.status_code = 404
         return "Track not found"
     return track
+
+@app.post('/tracks/',response_model = Track, status_code = 201)
+def create_track(track: Track):
+    track_dict = track.model_dump()
+    track_dict['id'] = max((t.id for t in data), default=0) + 1
+    new_track = Track(**track_dict)
+    data.append(new_track)
+    return new_track
+
+@app.put('/tracks/{track_id}' , response_model = Union[Track,str])
+def edit_track(track_id: int, updated_track: Track, response: Response):
+    track = None
+    for t in data:
+        if t.id == track_id:
+            track = t
+            break
+    if track is None:
+        response.status_code = 404
+        return "Track not found"
+    
+    for key, val in updated_track.model_dump().items():
+        if key != 'id':
+            setattr(track, key, val)
+    return track
+
+@app.delete('/tracks/{track_id}')
+def delete_track(track_id: int, response: Response):
+    track_index = None
+    for i,t in enumerate(data):
+        if t.id == track_id:
+            track_index = i
+            break
+    if track_index is None:
+        response.status_code = 404
+        return "Track not found"
+    data.pop(track_index)
+    return Response(status_code=200)
